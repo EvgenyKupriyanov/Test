58a5792067d019271cc7b6dbffab95a74944960f
Evgeny <geny_91@mail.ru>
2023-11-29 17:47:50 +0300

    Task

--- Test.cs
+++ Test.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача: Написать программу, которая из имеющегося массива строк формирует 
+//новый массив из строк, длина которых меньше, либо равна 3 символам. 
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте 
+// выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
+// лучше обойтись исключительно массивами.
+
+using System.Globalization;
+using System.Linq.Expressions;
+
+string[] array = new string[] { "Hello", "2", "world", ":-)" };
+int count = 0;
+for(int i = 0; i < array.Length; i++)
+{
+     if (array[i].Length <= 3)
+     {
+     count++;   
+     }
+}
+string[] arrayRes = new string[count];
+
+
+void ReleaseArray(string[] array, string[] arrayRes)
+{
+     int j = 0;
+     for(int i = 0; i < array.Length; i++)
+     {
+          if (array[i].Length <= 3)
+          {
+          arrayRes[j] = array[i];     
+          j++;   
+          }
+     }     
+}
+
+ReleaseArray(array, arrayRes);
+Console.WriteLine($"[{string.Join(", ", arrayRes)}]");
